5ee2dd0beaff13ff0fd8701e7188d1d608fa04cc
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-03-29 11:48:29 +0000

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,10 @@ import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import "./globals.css";
 import { useEffect } from 'react'
 
 import AOS from "aos";
-import "aos/dist/aos.css";
+import styles from "aos/dist/aos.module.css";
 
 export default function Home() {
   useEffect(() => {
