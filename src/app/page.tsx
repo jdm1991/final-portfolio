a5679202963f300407589324f9ba4386fd565429
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-04-01 12:24:12 +0100

    Latest fadeUp fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useInView } from "react-intersection-observer";
+import { useRef, useEffect, useState } from "react";
 
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
@@ -11,6 +12,12 @@ import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
 export default function Home() {
+  const [aboutPreviousInView, setAboutPreviousInView] = useState(false);
+  const [experiencePreviousInView, setExperiencePreviousInView] =
+    useState(false);
+  const [projectsPreviousInView, setProjectsPreviousInView] = useState(false);
+  const [contactPreviousInView, setContactPreviousInView] = useState(false);
+
   const [aboutRef, aboutInView] = useInView({
     rootMargin: "-200px 0px",
     triggerOnce: true,
@@ -18,7 +25,6 @@ export default function Home() {
     initialInView: false,
     threshold: 0.1,
     trackVisibility: true,
-    direction: "down", // Trigger only when scrolling down
   });
 
   const [experienceRef, experienceInView] = useInView({
@@ -28,7 +34,6 @@ export default function Home() {
     initialInView: false,
     threshold: 0.1,
     trackVisibility: true,
-    direction: "down", // Trigger only when scrolling down
   });
 
   const [projectsRef, projectsInView] = useInView({
@@ -38,7 +43,6 @@ export default function Home() {
     initialInView: false,
     threshold: 0.1,
     trackVisibility: true,
-    direction: "down", // Trigger only when scrolling down
   });
 
   const [contactRef, contactInView] = useInView({
@@ -48,8 +52,23 @@ export default function Home() {
     initialInView: false,
     threshold: 0.1,
     trackVisibility: true,
-    direction: "down", // Trigger only when scrolling down
   });
+
+  useEffect(() => {
+    setAboutPreviousInView(aboutInView);
+  }, [aboutInView]);
+
+  useEffect(() => {
+    setExperiencePreviousInView(experienceInView);
+  }, [experienceInView]);
+
+  useEffect(() => {
+    setProjectsPreviousInView(projectsInView);
+  }, [projectsInView]);
+
+  useEffect(() => {
+    setContactPreviousInView(contactInView);
+  }, [contactInView]);
 
   return (
     <div>
@@ -60,7 +79,9 @@ export default function Home() {
           id="About"
           ref={aboutRef}
           className={`${
-            aboutInView ? "animate-fade-up opacity-100" : "opacity-0"
+            aboutInView && !aboutPreviousInView
+              ? "animate-fade-up opacity-100"
+              : "opacity-0"
           } transition-opacity duration-2000`}
         >
           <About />
@@ -69,7 +90,9 @@ export default function Home() {
           id="Experience"
           ref={experienceRef}
           className={`${
-            experienceInView ? "animate-fade-up opacity-100" : "opacity-0"
+            experienceInView && !experiencePreviousInView
+              ? "animate-fade-up opacity-100"
+              : "opacity-0"
           } transition-opacity duration-2000`}
         >
           <Experience />
@@ -78,7 +101,9 @@ export default function Home() {
           id="Projects"
           ref={projectsRef}
           className={`${
-            projectsInView ? "animate-fade-up opacity-100" : "opacity-0"
+            projectsInView && !projectsPreviousInView
+              ? "animate-fade-up opacity-100"
+              : "opacity-0"
           } transition-opacity duration-2000`}
         >
           <Projects />
@@ -87,7 +112,9 @@ export default function Home() {
           id="Contact"
           ref={contactRef}
           className={`${
-            contactInView ? "animate-fade-up opacity-100" : "opacity-0"
+            contactInView && !contactPreviousInView
+              ? "animate-fade-up opacity-100"
+              : "opacity-0"
           } transition-opacity duration-2000`}
         >
           <Contact />
